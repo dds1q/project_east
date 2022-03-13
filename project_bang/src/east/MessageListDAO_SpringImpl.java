4cc57913166b61dc5c255dd6703ea20e5296238e
dadasiq <dadasiq@DESKTOP-4LS81EA>
2022-03-13 23:58:35 +0900

    update

--- project_bang/src/east/MessageListDAO_SpringImpl.java
+++ project_bang/src/east/MessageListDAO_SpringImpl.java
@@ -4,5 +4,11 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class MessageListDAO_SpringImpl implements MessageListDAO {
 	public JdbcTemplate jdbcTemplate = null;
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 }
